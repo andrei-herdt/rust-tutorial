a4e7a78f24f4eee1eccb258b78f2eb2a7cf03010
Andrei Herdt <andrei.herdt@tra.ai>
2019-11-16 22:21:41 +0000

    update chapter 3

--- chapter3/src/main.rs
+++ chapter3/src/main.rs
@@ -24,4 +24,38 @@ fn main() {
     println!("The tuple is x:{},y:{},z:{}", tup.0,tup.1,tup.2);
     let (x,y,z) = tup;
     println!("The tuple is x:{},y:{},z:{}", x,y,z);
+
+    let array = [1,2,3,4,5];
+    println!("The first element of array is {}", array[0]);
+
+    another_function(5);
+
+    let x = 3;
+    let y = {
+        let x = 4;
+        x + 1
+    };
+    println!("The value of y is {}", y);
+
+    println!("The return value of five is {}", five());
+
+    let array = [10, 20, 30, 40, 50];
+    let mut index = 0;
+    while index < 5 {
+        println!("The value at index {} is {}", index, array[index]);
+        index = index + 1;
+    }
+
+    for x in (1..4).rev() {
+        println!("x: {}", x);
+    }
+}
+
+fn another_function(x: i32)
+{
+    println!("The input to another_function is {}", x);
+}
+
+fn five () -> i32 {
+    5
 }
