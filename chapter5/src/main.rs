c4a9ce97b463a1c11343931352ecf06d0931f045
Andrei Herdt <andrei.herdt@tra.ai>
2019-11-19 21:13:59 +0000

    chapter 5

--- chapter5/src/main.rs
+++ chapter5/src/main.rs
@@ -7,6 +7,13 @@ fn main() {
     let area = compute_width(&rect);
 
     println!("The are of rect is: {}", area);
+
+    let rect1 = Rectangle { width: 30., height: 50. };
+    let rect2 = Rectangle { width: 10., height: 40. };
+    let rect3 = Rectangle { width: 60., height: 45. };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
 
 fn compute_width(r: &Rectangle) -> f32
@@ -17,4 +24,10 @@ fn compute_width(r: &Rectangle) -> f32
 struct Rectangle {
     width: f32,
     height: f32,
+}
+
+impl Rectangle{
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
